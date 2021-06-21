4d0b5806711dbb7604e4ac674de773f6bc36389b
alex180216 <marietsy.911.mm@gmail.com>
2021-06-21 18:51:57 -0300

    arreglo de menu portrait

--- components/Slider.js
+++ components/Slider.js
@@ -3,11 +3,7 @@ import { noCopy } from "../utils/noCopyUtils"
 const Slider = () =>{
     return(
         <section className="slider-section">
-            <div className="slider-hide"
-                onMouseDown={noCopy}
-                onSelect={noCopy}
-                onDragStart={noCopy}
-            >
+            <div className="slider-hide">
 
             </div>
         </section>
